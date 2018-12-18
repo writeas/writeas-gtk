0e912b37d5e531b1d4ec755e1261752f4bd40a23
Matt Baer <matt@write.as>
2018-12-17 20:35:48 -0500

    Don't move built CLI to data dir

--- build-cli.sh
+++ build-cli.sh
@@ -4,5 +4,4 @@ exec_name=writeas
 
 echo "Building $exec_name CLI..."
 gb build github.com/writeas/writeas-cli/cmd/writeas
-echo "mv bin/$exec_name data/$exec_name"
-mv bin/$exec_name data/$exec_name
+echo "Success."
