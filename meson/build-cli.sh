3cc852735d0d1396d14784feaaae615ac8a55cde
Christopher Davis <brainblasted@disroot.org>
2019-07-11 21:41:23 -0700

    build: Rework build script to build using go modules
    Instead of using the default go buildsystem with $GOPATH,
    the build script takes advantage of go modules pulling in
    the dependencies automatically with `go build`.

--- meson/build-cli.sh
+++ meson/build-cli.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 exec_name=writeas
+BIN_DIR="$MESON_BUILD_ROOT"/bin
 
 echo "Building $exec_name CLI..."
-go build github.com/writeas/writeas-cli/cmd/writeas
+cd "$MESON_SOURCE_ROOT"/src/github.com/writeas/writeas-cli/cmd/writeas &&
+mkdir -p "$BIN_DIR" &&
+go build -o "$BIN_DIR"/writeas &&
 echo "Success."
