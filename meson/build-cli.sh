507e6976e13ab72f8bc961ea01162859b43b9d0a
Christopher Davis <brainblasted@disroot.org>
2019-07-24 14:09:56 -0700

    Revert "build-cli: Use `go get` to fetch dependencies"
    This reverts commit 3faef3e108eb8d9847870009f44a933bd20415d4.

--- meson/build-cli.sh
+++ meson/build-cli.sh
@@ -4,8 +4,6 @@ exec_name=writeas
 BIN_DIR="$MESON_BUILD_ROOT"/bin
 
 echo "Building $exec_name CLI..."
-# go get to fetch dependencies
-go get github.com/writeas/writeas-cli/cmd/writeas &&
 cd "$MESON_SOURCE_ROOT"/src/github.com/writeas/writeas-cli/cmd/writeas &&
 mkdir -p "$BIN_DIR" &&
 go build -o "$BIN_DIR"/writeas &&
