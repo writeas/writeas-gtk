ad1249fff0e3499ffdf7a19da71ee9944ceb970e
Christopher Davis <brainblasted@disroot.org>
2020-08-26 20:21:28 -0700

    Build CLI with Go modules

--- meson/build-cli.sh
+++ meson/build-cli.sh
@@ -3,8 +3,8 @@
 exec_name=writeas
 
 echo "Building $exec_name CLI..."
-cd $1 &&
-gb build github.com/writeas/writeas-cli/cmd/writeas &&
+cd $1/src/github.com/writeas/writeas-cli/ && echo "$PWD"&&
+go build --mod=vendor -o bin/writeas ./cmd/writeas &&
 mv bin/writeas $3/$2 &&
 # Cleanup
 rm -rf bin/ &&
